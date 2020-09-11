67702980b4848dc47efeb96b31ac2d3e5a7fd13d
杨杰锋 <1179514691@qq.com>
2020-09-11 22:27:33 +0800

    面试题 01.09. 字符串轮转 word 程序

---
+++ 程序员面试金典中的题目/字符串/面试题 01.09. 字符串轮转/Solution3.java
@@ -0,0 +1,10 @@
+package com.yang.string;
+
+public class Solution3 {
+    public boolean isFlipedString(String s1, String s2) {
+        if(s1.length() != s2.length()) {
+            return false;
+        }
+        return (s2 + s2).contains(s1);
+    }
+}
