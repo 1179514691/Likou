7bbcb33ba238079783b388901568c4e38fdffde5
杨杰锋 <1179514691@qq.com>
2020-09-12 20:30:23 +0800

    面试题 10.05. 稀疏数组搜索 添加思路2 以及程序.

--- 程序员面试金典中的题目/二分查找/面试题 10.05. 稀疏数组搜索/Solution1.java
+++ 程序员面试金典中的题目/二分查找/面试题 10.05. 稀疏数组搜索/Solution1.java
@@ -18,4 +18,39 @@ public class Solution1 {
         return result;
     }
 
+    public int findString2(String[] words, String s) {
+
+        int hit = 0;
+
+        for (int i=0; i < words.length; i++) {
+            //统计不等于空的字符串
+            if (!words[i].equals("")) {
+                hit++;
+            }
+        }
+
+
+        String[][] remaining = new String[hit][2];
+        int index = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            //存储不等于空的字符串
+            if (!words[i].equals("")) {
+                remaining[index][0] = words[i];
+                remaining[index][1] = i+"";
+                index++;
+            }
+        }
+
+        //查找压缩过后的二维数组
+        for (int i = 0; i < hit; i++) {
+            if (s.equals(remaining[i][0])) {
+                return Integer.parseInt(remaining[i][1]);
+            }
+        }
+
+        return -1;
+
+    }
+
 }
