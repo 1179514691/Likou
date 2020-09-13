840f36732b9237245728bab1f47ed7077f083ab9
杨杰锋 <1179514691@qq.com>
2020-09-13 17:42:33 +0800

    面试题 02.01. 移除重复节点 把单独的ListNode分成两个java文件 方便自己重用ListNode

---
+++ 程序员面试金典中的题目/链表/面试题 02.01. 移除重复节点/Solution.java
@@ -0,0 +1,57 @@
+package com.yang.linkedList;
+
+import java.util.HashSet;
+
+public class Solution {
+
+    public ListNode removeDuplicateNodes(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(head.val);
+
+        ListNode temp = head;
+
+        while(temp.next != null) {
+            if (set.add(temp.next.val)) {
+                temp = temp.next;
+            }else {
+                temp.next = temp.next.next;
+            }
+
+        }
+
+
+        return head;
+    }
+
+    public ListNode removeDuplicateNodes2(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode temp = head;
+
+        HashSet<Integer> set =new HashSet<>();
+
+        set.add(head.val);
+
+
+        while (temp.next != null) {
+            if (set.contains(temp.next.val)) {
+                temp.next = temp.next.next;
+
+            }else {
+                set.add(temp.next.val);
+                temp = temp.next;
+            }
+        }
+
+        return head;
+    }
+}
