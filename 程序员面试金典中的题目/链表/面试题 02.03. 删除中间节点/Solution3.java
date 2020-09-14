31fd59f62c1ba28b186c0639112a5d35c5d81ddc
杨杰锋 <1179514691@qq.com>
2020-09-14 22:13:15 +0800

    面试题 02.03. 删除中间节点 word文档(包含图解) 源码程序

---
+++ 程序员面试金典中的题目/链表/面试题 02.03. 删除中间节点/Solution3.java
@@ -0,0 +1,8 @@
+package com.yang.linkedList;
+
+public class Solution3 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
