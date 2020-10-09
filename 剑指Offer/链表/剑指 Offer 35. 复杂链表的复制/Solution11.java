ba470b737d695ea4ddb205dd4af61d96b65f51cd
杨杰锋 <1179514691@qq.com>
2020-10-09 16:08:24 +0800

    剑指 Offer 35. 复杂链表的复制 添加另一个解法

--- 剑指Offer/链表/剑指 Offer 35. 复杂链表的复制/Solution11.java
+++ 剑指Offer/链表/剑指 Offer 35. 复杂链表的复制/Solution11.java
@@ -42,4 +42,52 @@ public class Solution11 {
         return map.get(head);
 
     }
+
+    public Node copyRandomList2(Node head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        Node temp = head;
+
+        //复制链表节点
+        while(temp != null) {
+            Node node = new Node(temp.val);
+            node.next = temp.next;
+            temp.next = node;
+            temp = temp.next.next;
+        }
+
+        //复制链表节点的随机指针
+        temp = head;
+        while(temp != null) {
+
+            //判断原来的节点是否没有random
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
+            }
+
+            temp = temp.next.next;
+        }
+
+        //把链表一分为二
+        Node copyHead = head.next;
+        temp = head;
+        Node tempCopy = head.next;
+
+        while(temp != null) {
+
+            temp.next = temp.next.next;
+            temp = temp.next;
+
+            if (tempCopy.next != null) {
+                tempCopy.next = tempCopy.next.next;
+                tempCopy = tempCopy.next;
+            }
+        }
+
+        return copyHead;
+
+    }
 }
