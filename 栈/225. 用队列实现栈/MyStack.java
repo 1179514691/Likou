8f44f85e5792921e5c92cec8a71a099564850e40
杨杰锋 <1179514691@qq.com>
2020-11-09 11:39:49 +0800

    225. 用队列实现栈 word文档 源程序

---
+++ 栈/225. 用队列实现栈/MyStack.java
@@ -0,0 +1,35 @@
+package com.liko.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class MyStack {
+
+    private Deque<Integer> queue;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new ArrayDeque<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.add(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.pollLast();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peekLast();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
