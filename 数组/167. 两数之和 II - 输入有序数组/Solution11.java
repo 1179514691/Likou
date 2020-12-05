c7c4bcc0dd0da5abe2e9fe7fe326885a5332d363
杨杰锋 <1179514691@qq.com>
2020-12-05 14:11:17 +0800

    167. 两数之和 II - 输入有序数组 word文档 源程序

---
+++ 数组/167. 两数之和 II - 输入有序数组/Solution11.java
@@ -0,0 +1,32 @@
+package com.liko.array;
+
+public class Solution11 {
+    public int[] twoSum(int[] numbers, int target) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[]{i+1 , j+1};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    public int[] twoSum2(int[] numbers, int target) {
+        int left = 0;
+        int right =numbers.length -1;
+
+        while(left < right) {
+            if (numbers[left]+numbers[right] < target) {
+                left++;
+            }else if (numbers[left]+numbers[right] > target) {
+                right--;
+            }else {
+                return new int[]{left+1 , right+1};
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
